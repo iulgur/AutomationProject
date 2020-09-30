ab7697e1f9f05683808b48f45d3b0e267a33db37
Ilyas Ulgur <ulgur@yahoo.com>
2020-09-30 22:42:16 +0200

    intial comment

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,10 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        System.out.println("Hello Java! EU3");
+        System.out.println("My name is Ilyas");
+
+        //some more changes are coming...
+        //i am ready now...
     }
 }
